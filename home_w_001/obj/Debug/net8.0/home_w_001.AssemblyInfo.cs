646829db2c3c7ad62e2f7f49b61b6d47dce3cf9b
Polina <marinini.po@gmail.com>
2023-12-04 17:54:04 +0300

    home_w_001

--- home_w_001/obj/Debug/net8.0/home_w_001.AssemblyInfo.cs
+++ home_w_001/obj/Debug/net8.0/home_w_001.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("home_w_001")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfa6892b23b6560b092ebf598f59c8e3b75b6b18")]
 [assembly: System.Reflection.AssemblyProductAttribute("home_w_001")]
 [assembly: System.Reflection.AssemblyTitleAttribute("home_w_001")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
